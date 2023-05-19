d1849901f41592bf634a74d30dbf79c98d8e7547
ig-or-ka <mish.dubov@gmail.com>
2023-05-20 02:43:36 +0300

    Реализовал расходную накладную

--- main.py
+++ main.py
@@ -7,6 +7,61 @@ import pandas as pd
 from datetime import datetime
 
 
+class Report(QtWidgets.QDialog):
+    def __init__(self, parent, df:pd.DataFrame):
+        super(Report, self).__init__(parent)
+        self.df = df
+        uic.loadUi('report.ui', self)
+        self.show()
+        self.load_table()
+
+    def load_table(self):
+        table: QTableWidget = self.products
+        table.setColumnCount(5)
+        table.verticalHeader().setVisible(False)
+
+        table.setHorizontalHeaderLabels(['Индекс','Название','Цена','Количество','Сумма'])
+
+        index_filial_id = self.df['index'].count()-1
+        filial_id = int(self.df['index'].loc[self.df.index[index_filial_id]])
+        self.filial_id.setText(f"Номер филиала: {filial_id}")
+
+        all_sum = 0
+
+        for i in range(self.df['index'].count()-1):
+            index = int(self.df['index'].loc[self.df.index[i]])
+            name = self.df['name'].loc[self.df.index[i]]
+            price = int(self.df['price'].loc[self.df.index[i]])
+            count = int(self.df['count'].loc[self.df.index[i]])
+
+            all_sum += count*price
+
+            table.insertRow(i)
+       
+            item_index = QTableWidgetItem(str(index))
+            item_index.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            table.setItem(i,0, item_index)
+
+            item_name = QTableWidgetItem(name)
+            item_name.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            table.setItem(i,1, item_name)
+
+            item_price = QTableWidgetItem(str(price))
+            item_price.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            table.setItem(i,2, item_price)
+
+            item_count = QTableWidgetItem(str(count))
+            item_count.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            table.setItem(i,3, item_count)
+
+            item_sum = QTableWidgetItem(str(count*price))
+            item_sum.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            table.setItem(i,4, item_sum)
+
+        table.resizeColumnsToContents()
+
+        self.all_sum.setText(f"Итого: {all_sum}")
+
 def create_table(db):
     db.execute("""CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -152,11 +207,26 @@ class MainStorage:
         self.ui.back.clicked.connect(self.ui.start_ui)
 
         self.ui.move_to_filial_cklick = self.move_to_filial_cklick
-        self.ui.move_to_filial.clicked.connect(self.ui.move_to_filial_cklick)    
+        self.ui.move_to_filial.clicked.connect(self.ui.move_to_filial_cklick)
+
+        self.ui.read_inv_click = self.read_inv_click
+        self.ui.read_inv.clicked.connect(self.ui.read_inv_click)
 
     
     def move_to_filial_cklick(self):
         move_or_sell(self)
+
+
+    def read_inv_click(self):
+        dialog = QFileDialog(self.ui)
+        dialog.setFileMode(QFileDialog.FileMode.AnyFile)
+        dialog.setNameFilter("CSV (*.csv)")
+
+        if dialog.exec():
+            filename = dialog.selectedFiles()[0]
+            df = pd.read_csv(filename)
+
+            Report(self.ui, df)
 
 
 class Filial:
@@ -193,7 +263,19 @@ class Filial:
             QMessageBox.about(self.ui, "Ошибка", f"Не совершено ни одной покупки!")
 
         else:
-            pass
+            products = dict()
+
+            for sell in self.sells_to_export:
+                prod = products.setdefault(sell['index'], {'index':sell['index'],'name':sell['name'],'price':sell['price'],'count':0})
+                prod['count'] += sell['count']
+
+            products[-10] = {'index':self.filial_id,'name':'Филиал','price':'','count':''}
+            df = pd.DataFrame(products.values())
+            tm = datetime.now().strftime("%d.%m.%y %H.%M")
+            df.to_csv(f"files/Отчет Филиал {self.filial_id} {tm}.csv")
+
+            self.sells_to_export = []
+            QMessageBox.about(self.ui, "Готово", f"Отчет сформирован!")            
 
 
     def receipt_invoice_click(self):
