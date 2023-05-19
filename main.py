4e84b9dd09039dfe20628e608fee3d0c2e6e11ce
ig-or-ka <mish.dubov@gmail.com>
2023-05-20 00:57:13 +0300

    Реализовал импорт накладных

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMessageBox, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QMessageBox, QTableWidget, QTableWidgetItem, QFileDialog
 import sys
 import sqlite3
 import pandas as pd
 from datetime import datetime
 
 
+def create_table(db):
+    db.execute("""CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        name TEXT NOT NULL,
+        price INT NOT NULL,
+        count INT NOT NULL
+    );""")
+
+    db.commit()
+
+
+def get_all_products(db):
+    cur = db.execute("select * from products")
+    return cur.fetchall()
+
+
+def update_products_table(prog):
+    table: QTableWidget = prog.ui.select_products
+    table.clear()
+    table.setColumnCount(6)
+    table.verticalHeader().setVisible(False)
+
+    labels = ['','Индекс','Название','Цена','Общее количество']
+
+    if type(prog) == MainStorage:
+        labels.append('Количество для перемещения')
+
+    else:
+        labels.append('Количество для продажи')
+
+    table.setHorizontalHeaderLabels(labels)
+
+    products = get_all_products(prog.db)
+    for i, product in enumerate(products):
+        table.insertRow(i)
+
+        item_check = QTableWidgetItem()
+        item_check.data(Qt.ItemDataRole.CheckStateRole)
+        item_check.setCheckState(Qt.CheckState.Unchecked)
+        table.setItem(i,0, item_check)
+    
+        item_index = QTableWidgetItem(str(product[0]))
+        item_index.setFlags(Qt.ItemFlag.ItemIsEnabled)
+        table.setItem(i,1, item_index)
+
+        item_name = QTableWidgetItem(product[1])
+        item_name.setFlags(Qt.ItemFlag.ItemIsEnabled)
+        table.setItem(i,2, item_name)
+
+        item_price = QTableWidgetItem(str(product[2]))
+        item_price.setFlags(Qt.ItemFlag.ItemIsEnabled)
+        table.setItem(i,3, item_price)
+
+        item_count = QTableWidgetItem(str(product[3]))
+        item_count.setFlags(Qt.ItemFlag.ItemIsEnabled)
+        table.setItem(i,4, item_count)
+
+        table.setItem(i,5, QTableWidgetItem("0"))
+
+    table.resizeColumnsToContents()
+
+
 class MainStorage:
     def __init__(self, ui):
-        self.db = sqlite3.connect("main_storage.db")
-        self.create_tables()
+        self.db = sqlite3.connect("files/main_storage.db")
+        create_table(self.db)
         self.ui = ui
         self.init_ui()
 
@@ -22,7 +84,7 @@ class MainStorage:
         self.ui.move_to_filial_cklick = self.move_to_filial_cklick
         self.ui.move_to_filial.clicked.connect(self.ui.move_to_filial_cklick)
 
-        self.update_products_table()
+        update_products_table(self)
 
     
     def move_to_filial_cklick(self):
@@ -73,68 +135,73 @@ class MainStorage:
             tm = datetime.now().strftime("%d.%m.%y %H.%M")
             df.to_csv(f'files/Перемещение в филиал {tm}.csv')
 
-            self.update_products_table()
+            update_products_table(self)
 
             QMessageBox.about(self.ui, "Готово", f'Документ "Перемещение в филиал" успешно сформирован!')
-
-
-    def update_products_table(self):
-        table: QTableWidget = self.ui.select_products
-        table.clear()
-        table.setColumnCount(6)
-        table.verticalHeader().setVisible(False)
-        table.setHorizontalHeaderLabels(['','Индекс','Название','Цена','Общее количество','Количество для перемещения'])
-
-        products = self.get_all_products()
-        for i, product in enumerate(products):
-            table.insertRow(i)
-
-            item_check = QTableWidgetItem()
-            item_check.data(Qt.ItemDataRole.CheckStateRole)
-            item_check.setCheckState(Qt.CheckState.Unchecked)
-            table.setItem(i,0, item_check)
-        
-            item_index = QTableWidgetItem(str(product[0]))
-            item_index.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            table.setItem(i,1, item_index)
-
-            item_name = QTableWidgetItem(product[1])
-            item_name.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            table.setItem(i,2, item_name)
-
-            item_price = QTableWidgetItem(str(product[2]))
-            item_price.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            table.setItem(i,3, item_price)
-
-            item_count = QTableWidgetItem(str(product[3]))
-            item_count.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            table.setItem(i,4, item_count)
-
-            table.setItem(i,5, QTableWidgetItem("0"))
-
-        table.resizeColumnsToContents()
-
-
-    def create_tables(self):
-        self.db.execute("""CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            name TEXT NOT NULL,
-            price INT NOT NULL,
-            count INT NOT NULL
-        );""")
-
-        self.db.commit()
-        
-
-    def get_all_products(self):
-        cur = self.db.execute("select * from products")
-        return cur.fetchall()
 
 
 class Filial:
     def __init__(self, ui, filial_id):
         self.filial_id = filial_id
         self.ui = ui
+        self.db = sqlite3.connect(f"files/filial_{filial_id}.db")
+        create_table(self.db)
+        self.init_ui()
+        update_products_table(self)
+
+
+    def init_ui(self):
+        uic.loadUi('filial.ui', self.ui)
+        self.ui.back.clicked.connect(self.ui.start_ui)
+
+        self.ui.receipt_invoice_click = self.receipt_invoice_click
+        self.ui.receipt_invoice.clicked.connect(self.ui.receipt_invoice_click)
+
+
+    def receipt_invoice_click(self):
+        dialog = QFileDialog(self.ui)
+        dialog.setFileMode(QFileDialog.FileMode.AnyFile)
+        dialog.setNameFilter("CSV (*.csv)")
+
+        if dialog.exec():
+            to_rewrite_prices = []
+
+            filename = dialog.selectedFiles()[0]
+            df = pd.read_csv(filename)
+
+            for i in range(df['index'].count()):
+                index = int(df['index'].loc[df.index[i]])
+                name = df['name'].loc[df.index[i]]
+                price = int(df['price'].loc[df.index[i]])
+                count = int(df['count'].loc[df.index[i]])
+
+                cur = self.db.execute("select * from products where id=?",(index,))
+                res = cur.fetchone()
+
+                if res == None:
+                    self.db.execute(
+                        "insert into products (id, name, price, count) values (?,?,?,?)", 
+                        (index, name, price, count)
+                    )
+
+                else:
+                    if res[2] != price:
+                        to_rewrite_prices.append({'index':index, 'name':name, 'price':price})
+
+                    self.db.execute("update products set price=?, count=? where id=?",(price,res[3]+count,index))
+                
+            self.db.commit()
+            
+            if len(to_rewrite_prices) > 0:
+                df = pd.DataFrame(to_rewrite_prices)
+                tm = datetime.now().strftime("%d.%m.%y %H.%M")
+                df.to_csv(f'files/Изменение цен филиал {self.filial_id} {tm}.csv')
+
+                QMessageBox.about(self.ui, "Изменение", f'Сформирован список изменения цен')
+
+            update_products_table(self)
+
+            QMessageBox.about(self.ui, "Готово", f'Накладная обработана!')
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -163,7 +230,7 @@ class Ui(QtWidgets.QMainWindow):
     def enter_filial_id(self):
         try:
             filial_id = int(self.filial_id_text.text())
-            QMessageBox.about(self, "Ok", str(filial_id))
+            Filial(self,filial_id)
 
         except:
             QMessageBox.about(self, "Error", "Введите число")
